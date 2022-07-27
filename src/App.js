28f5cfa563a9835ae2d3615db20a72e561bf1f74
lizayao <lizayao95@hotmail.com>
2022-07-27 19:12:58 -0300

    d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import img from "./images/logo.png"
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <NavBar className="NavBar"></NavBar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lalalisa
-        </a>
+        <img src={img} alt="logo" />
+
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import "./NavBar.css";
 export default function NavBar(){
     return(
         <ul>
+            <a><img src="./images/logo.png" alt="Nail Store"></img></a>
             <a><li>Inicio</li></a>
             <a><li>Insumos</li></a>
             <a><li>Contacto</li></a>
