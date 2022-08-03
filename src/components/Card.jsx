15e7c29cae26b719c47f3abd7ab28702f792744f
lizayao <lizayao95@hotmail.com>
2022-08-02 21:25:52 -0300

    holis

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+function Card(){
+    return(
+      <div className="main">
+        <div className="card">
+          <div className="card-img">
+            <img src="https://picsum.photos/280/260" alt="imagen" />
+          </div>
+          <div className="card-detail">
+            <h2>Producto</h2>
+            <p>
+              lorem ipsum dolor sit.
+            </p>
+            <h3>$ 1090</h3>
+            <button>Ver más</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
