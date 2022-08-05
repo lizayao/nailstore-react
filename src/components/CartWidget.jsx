1c38eec86e4c7fe4393289cac82b09e60b390079
lizayao <lizayao95@hotmail.com>
2022-08-04 21:43:04 -0300

    cart

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./style.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-/* import { fa-solid fa-cart-shopping }  from "@fortawesome/free-solid-svg-icons"; */
-import { faCoffee }  from "@fortawesome/free-solid-svg-icons";
-import * as iconList from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping }  from "@fortawesome/free-solid-svg-icons";
 
 function CartWidget(){
     return(
         <div className="carrito">
-            {/* <FontAwesomeIcon icon="fa-solid fa-cart-shopping" /> */}
-            <a href="#"><FontAwesomeIcon icon={ faCoffee } /></a>
+            <a href="#"><FontAwesomeIcon icon={ faCartShopping } /></a>
         </div>
     )
 }
