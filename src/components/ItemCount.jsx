92319d599905103deefdcd933852c92d968b05e6
lizayao <lizayao95@hotmail.com>
2022-08-08 23:26:35 -0300

    fgdfg

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import "./style.css"
 
 function ItemCount(props) {
   const [clicks, setClicks] = React.useState(1);
 
-  const handleIncrement = () => {
+  const sumarProd = () => {
     //condicion q el click no supere el stock
-    setClicks(clicks+1)
+    setClicks(clicks + 1)
     /* (clicks = stock) ? (setClicks(clicks+1)) : setClicks(clicks+0) */
   };
 
-  const handleDecrement = () => {
-    setClicks(clicks-1)
+  const restarProd = () => {
+    setClicks(clicks - 1)
   };
 
   const onAdd = () => {
@@ -18,12 +19,12 @@ function ItemCount(props) {
   };
 
   return (
-    <>
-      <h3>Item Counter</h3>
-      <button onClick={handleDecrement}>restar</button>
-      <button onClick={handleIncrement}>sumar</button>
+    <div className="contador">
+      <button onClick={restarProd}> - </button>
+      <p>{clicks}</p>
+      <button onClick={sumarProd}> + </button>
       <button onClick={onAdd}>Agregar</button>
-    </>
+    </div>
   );
 }
 
